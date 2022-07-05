17031ab1ef4cf0c06dad60d0a458f57d197c60a0
Paul Broadwith <paul@pauby.com>
2022-07-05 17:45:13 +0100

    rebase

--- Ansible and Windows/script.ps1
+++ Ansible and Windows/script.ps1
@@ -1,18 +1,16 @@
 # Let's run through the code
-rm ls /mnt/c/psconfeu2022
+rm /mnt/c/psconfeu2022 -rf
 
 # Set the correct directory first
-cd Ansible\ and\ Windows
-
-. cred-secret.sh
-
-cd ansible
+cd Ansible\ and\ Windows && . cred-secret.sh && cd ansible
 
 # Update the hosts file with IP address
 ansible -m win_ping psconfeu2022 -i hosts -e "ansible_user=$username ansible_password=$pwd"
 
 # Copy the module - normally you'd use the ansible-galaxy command
 /usr/bin/cp ../../../ansible_collection_pauby.psconfeu2022/. ~/.ansible/collections/ansible_collections/pauby/psconfeu2022/ -r -f
+
+ls /mnt/c/psconfeu2022
 
 # Execute the playbook
 ansible-playbook playbook.yml -i hosts -e "ansible_user=$username ansible_password=$pwd"
